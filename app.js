dd7493433f22b0b092f78845f1a13393d80bd1a4
Austin Lauritsen <austin@veryaustin.com>
2017-03-23 15:57:33 -0500

    Set static directories and install bootstrap template

--- app.js
+++ app.js
@@ -5,6 +5,20 @@ var app = express();
 
 // Set Port
 var port = 5000;
+
+// Setup Static Directories
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+app.get('/', function (req, res) {
+  res.send('Hello World');
+});
+
+
+app.get('/books', function (req, res) {
+  res.send('Hello Books');
+});
+
 app.listen(port, function (err) {
   console.log('running server on port ' + port);
 });

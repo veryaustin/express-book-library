86994e51075145d2871dad107210fe6e7270a728
Austin Lauritsen <austin@veryaustin.com>
2017-03-24 13:40:43 -0500

    Comment out sample data

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -2,50 +2,51 @@ var express = require('express');
 var adminRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;
 
-var books = [
-  {
-    title: 'War and Peace',
-    genre: 'Historical Fiction',
-    author: 'Lev Nikolayevich Tolstoy',
-    read: false
-  },
-  {
-    title: 'Les Miserables',
-    genre: 'Historical Fiction',
-    author: 'Victor Hugo',
-    read: false
-  },
-  {
-    title: 'The Time Machine',
-    genre: 'Science Fiction',
-    author: 'H. G. Wells',
-    read: false
-  },
-  {
-    title: 'A Journey into the Center of the Earth',
-    genre: 'Science Fiction',
-    author: 'Jules Verne',
-    read: false
-  },
-  {
-    title: 'The Dark World',
-    genre: 'Fantasy',
-    author: 'Henry Kuttner',
-    read: false
-  },
-  {
-    title: 'The Wind in the Willows',
-    genre: 'Fantasy',
-    author: 'Kenneth Grahame',
-    read: false
-  },
-  {
-    title: 'Life On The Mississippi',
-    genre: 'History',
-    author: 'Mark Twain',
-    read: false
-  }
-];
+// Sample Data
+// var books = [
+//   {
+//     title: 'War and Peace',
+//     genre: 'Historical Fiction',
+//     author: 'Lev Nikolayevich Tolstoy',
+//     read: false
+//   },
+//   {
+//     title: 'Les Miserables',
+//     genre: 'Historical Fiction',
+//     author: 'Victor Hugo',
+//     read: false
+//   },
+//   {
+//     title: 'The Time Machine',
+//     genre: 'Science Fiction',
+//     author: 'H. G. Wells',
+//     read: false
+//   },
+//   {
+//     title: 'A Journey into the Center of the Earth',
+//     genre: 'Science Fiction',
+//     author: 'Jules Verne',
+//     read: false
+//   },
+//   {
+//     title: 'The Dark World',
+//     genre: 'Fantasy',
+//     author: 'Henry Kuttner',
+//     read: false
+//   },
+//   {
+//     title: 'The Wind in the Willows',
+//     genre: 'Fantasy',
+//     author: 'Kenneth Grahame',
+//     read: false
+//   },
+//   {
+//     title: 'Life On The Mississippi',
+//     genre: 'History',
+//     author: 'Mark Twain',
+//     read: false
+//   }
+// ];
 
 var router = function (nav) {
   adminRouter.route('/addBooks')
